228afad440d31d48ffbb020351780eb29b37d43d
vxl3 <120146587+vxl3@users.noreply.github.com>
2023-01-05 11:46:49 +0300

    Update config.py

--- VCsMusicBot/config.py
+++ VCsMusicBot/config.py
@@ -8,18 +8,18 @@ if os.path.exists("config.py"):
 
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("5837427920:AAEHQ8DYhGiTusz2rpz6s_T6BlD9lJKQZ4M")
-BOT_NAME = getenv("𝑫𝑬𝑵𝑨𝑹")
+BOT_TOKEN = "5837427920:AAEHQ8DYhGiTusz2rpz6s_T6BlD9lJKQZ4M"
+BOT_NAME = "𝑫𝑬𝑵𝑨𝑹"
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "tgbotproject")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/cf19dda907391656338eb.png")
 admins = {}
-API_ID = int(getenv("29699592", ""))
-API_HASH = getenv("36c5d87a0d72b145a014b89def282cbd")
-BOT_USERNAME = getenv("D3NARBOT")
+API_ID = "29699592"
+API_HASH = "36c5d87a0d72b145a014b89def282cbd"
+BOT_USERNAME = "D3NARBOT"
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "VCsMusicPlayer")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "zauteschat")
 PROJECT_NAME = getenv("PROJECT_NAME", "VCsMusicBot")
-SOURCE_CODE = getenv("SOURCE_CODE", "github.com/ZauteKm/VCsMusicBot")
+SOURCE_CODE = "https://github.com/vxl3/VCsMusicBot"
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
