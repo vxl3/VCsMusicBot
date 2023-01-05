9d77b359db67d89c690ac4398afcf6153dddf8af
vxl3 <120146587+vxl3@users.noreply.github.com>
2023-01-05 11:37:10 +0300

    Update config.py

--- VCsMusicBot/config.py
+++ VCsMusicBot/config.py
@@ -8,14 +8,14 @@ if os.path.exists("local.env"):
 
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+BOT_TOKEN = getenv("5837427920:AAEHQ8DYhGiTusz2rpz6s_T6BlD9lJKQZ4M")
+BOT_NAME = getenv("𝑫𝑬𝑵𝑨𝑹")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "tgbotproject")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/cf19dda907391656338eb.png")
 admins = {}
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME")
+API_ID = int(getenv("29699592", ""))
+API_HASH = getenv("36c5d87a0d72b145a014b89def282cbd")
+BOT_USERNAME = getenv("D3NARBOT")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "VCsMusicPlayer")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "zauteschat")
 PROJECT_NAME = getenv("PROJECT_NAME", "VCsMusicBot")
